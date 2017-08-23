c22d9caf1f2765f1178f24b2fd31c063b163b5e3
Skalecka <nightlinx@users.noreply.github.com>
2017-08-23 15:41:59 +0200

    About Component

--- angular/bonanza/src/app/app.module.ts
+++ angular/bonanza/src/app/app.module.ts
@@ -4,21 +4,29 @@ import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { ItemService } from './services/item.service';
 import { HttpModule }    from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // Imports for loading & configuring the in-memory web api
 // it is replacing the Http client's XHR backend service with an in-memory alternative.
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-
 //InMemoryDataService class primes the in-memory database.
 import { InMemoryDataService }  from './services/in-memory-data.service';
 
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path:'about', component:AboutComponent}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
