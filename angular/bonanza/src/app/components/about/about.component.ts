1046994b0f97516a43b29fecefde6c93d0cf045b
Skalecka <nightlinx@users.noreply.github.com>
2017-08-24 17:30:14 +0200

    Change gif after click

--- angular/bonanza/src/app/components/about/about.component.ts
+++ angular/bonanza/src/app/components/about/about.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  imageSource: any;
+  imageSources: string[];
+  index: number;
+  constructor() {
+    this.index = 0;
+    this.imageSource = "../../../assets/star_mario.gif";
+    this.imageSources =[
+      "../../../assets/star_mario.gif",
+      "../../../assets/yoshi.gif",
+      "../../../assets/mario.gif",
+      "../../../assets/grzyb_mario.gif",
+      "../../../assets/kirby.gif",
+      "../../../assets/donkey_kong.gif",
+     ];
+  }
 
   ngOnInit() {
   }
 
+  changeImage(image:any) {
+    if(this.index < this.imageSources.length - 1){
+      this.index = this.index + 1;
+    }
+    else{
+      this.index = 0;
+    }
+    this.imageSource = this.imageSources[this.index];
+  }
 }
